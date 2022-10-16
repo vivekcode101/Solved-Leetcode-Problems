25f4482820f9c3610b9e17ac5552bd1e07dcb8dd
Vivek Yadav <vivekcode101@gmail.com>
2022-10-16 13:25:23 +0530

    Time: 85 ms (37.77%), Space: 36.1 MB (70.35%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -5,14 +5,14 @@ public:
         int count =0;
         for(int i = 0; i<nums.size();i++)
         {
-            if(nums[i]==0)
-            {
-                count =0;
-            }
-            else
+            if(nums[i]==1)
             {
                 count++;
                 result = max(result,count);
+            }
+            else
+            {
+                count=0;
             }
         }
         return result;
