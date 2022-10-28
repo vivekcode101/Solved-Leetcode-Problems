ee875a75186f0c0af273f244a8283b654ea6ec11
Vivek Yadav <vivekcode101@gmail.com>
2022-10-28 20:20:09 +0530

    Time: 12 ms (22.48%), Space: 9.6 MB (51.52%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,7 +5,7 @@ public:
         int col = matrix[0].size();
         int start = 0;
         int end = row*col-1;
-        int mid = (start+end)/2;
+        int mid = start + (end-start)/2;
         while(start<=end)
         {
             int element = matrix[mid/col][mid%col];
@@ -22,7 +22,7 @@ public:
                 start = mid+1;
                 
             }
-            mid = (start+end)/2;
+            mid = start+(end-start)/2;
         }
          return 0; 
     }
