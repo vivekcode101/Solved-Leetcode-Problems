c23c6c409b172d4548c4dd3106a4d6379bc327b5
Vivek Yadav <vivekcode101@gmail.com>
2022-11-26 15:14:31 +0530

    Time: 3 ms (92.38%), Space: 11.1 MB (75.45%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -10,18 +10,33 @@ public:
         while(low <= high){
             
             mid = low + (high-low)/2;
-            if(nums[mid] == target) return  mid;
+            if(nums[mid] == target)
+            {
+                return  mid;
+            }
             if(nums[mid] >= nums[low])
             {
                 /* left half sorted */
-                if(target >= nums[low] && target < nums[mid]) high = mid-1;
-                else low = mid+1;
+                if(target >= nums[low] && target < nums[mid])
+                {
+                    high = mid-1;
+                }
+                else
+                {
+                    low = mid+1;
+                }
             }
             else
             {
                 /* right half is sorted */
-                if( target > nums[mid] && target <= nums[high]) low = mid+1;
-                else high = mid-1;
+                if( target > nums[mid] && target <= nums[high])
+                {
+                    low = mid+1;
+                }
+                else
+                {
+                    high = mid-1;
+                }
             }
         }
         return -1;
