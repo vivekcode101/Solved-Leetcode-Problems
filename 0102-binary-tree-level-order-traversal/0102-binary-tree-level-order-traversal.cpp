7b45bed7be933008332210dc2544cf5c18c43c59
Vivek Yadav <vivekcode101@gmail.com>
2023-04-15 13:23:38 +0530

    Time: 3 ms (82.05%), Space: 12.5 MB (65.35%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,21 +11,73 @@
  */
 class Solution {
 public:
-   vector<vector<int>> ret;
+//    vector<vector<int>> ret;
 
-void buildVector(TreeNode *root, int depth)
-{
-    if(root == NULL) return;
-    if(ret.size() == depth)
-        ret.push_back(vector<int>());
+// void buildVector(TreeNode *root, int depth)
+// {
+//     if(root == NULL) return;
+//     if(ret.size() == depth)
+//         ret.push_back(vector<int>());
     
-    ret[depth].push_back(root->val);
-    buildVector(root->left, depth + 1);
-    buildVector(root->right, depth + 1);
-}
+//     ret[depth].push_back(root->val);
+//     buildVector(root->left, depth + 1);
+//     buildVector(root->right, depth + 1);
+// }
 
-vector<vector<int> > levelOrder(TreeNode *root) {
-    buildVector(root, 0);
-    return ret;
+// vector<vector<int> > levelOrder(TreeNode *root) {
+//     buildVector(root, 0);
+//     return ret;
+// }
+    vector<vector<int> > levelOrder(TreeNode *root) {
+    
+//     vector<vector<int>> ans;
+        
+//         queue< TreeNode *>q;
+//         q.push(root);
+//         while(!q.empty())
+//         {
+//             TreeNode *temp=q.front();
+//             ans.push_back(temp->val);
+//             q.pop();
+
+//             if(temp->left)
+//             {
+//                 q.push(temp->left);
+//             }
+//             if(temp->right)
+//             {
+//                 q.push(temp->right);
+//             }
+//         }
+        
+//         return ans;
+        if(!root)
+            return {};
+        vector<vector<int>> ans;
+        queue<TreeNode *>q;
+        
+        q.push(root);
+        while(!q.empty())
+        {
+            int hps = q.size();
+            vector<int>lot;
+            for(int i = 0;i<hps;i++)
+            {                
+                TreeNode *temp=q.front();
+                q.pop();
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }                 
+                if(temp->right)
+                 {
+                q.push(temp->right);
+                 }
+                
+                lot.push_back(temp->val);
+            }
+            ans.push_back(lot);
+        }
+        return ans;
 }
 };
