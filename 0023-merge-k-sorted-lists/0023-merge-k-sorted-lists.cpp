5fe2665e8a6284742afbdc7ea034d149f7c3aaac
Vivek Yadav <vivekcode101@gmail.com>
2023-05-30 16:42:42 +0530

    Time: 25 ms (69.64%), Space: 13.2 MB (82.77%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    class compare
+    {
+        public:
+        bool operator()(ListNode*a,ListNode*b)
+        {
+            return a->val>b->val;
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,compare>minheap;
+        int k = lists.size();
+        if(k==0)
+        {
+            return NULL;
+        }
+        for(int i = 0;i<k;i++)
+        {
+            if(lists[i]!=NULL)
+            {
+                minheap.push(lists[i]);
+            }
+        }
+        ListNode*head=NULL;
+        ListNode*tail=NULL;
+        while(minheap.size()>0)
+        {
+            ListNode*top=minheap.top();
+            minheap.pop();
+            if(top->next!=NULL)
+            {
+                minheap.push(top->next);
+            }
+            if(head==NULL)
+            {
+                head=top;
+                tail=top;
+            }
+            else
+            {
+                tail->next=top;
+                tail=top;
+            }
+        }
+        return head;
+    }
+};
