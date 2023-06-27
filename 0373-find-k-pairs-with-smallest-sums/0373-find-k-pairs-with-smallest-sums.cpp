62ca69784638bf239245ce68a8de8cbdecc60d15
Vivek Yadav <vivekcode101@gmail.com>
2023-06-27 17:47:03 +0530

    Time: 405 ms (25.38%), Space: 109.8 MB (81.11%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-          vector<vector<int>> ans;
-        priority_queue<pair<int,pair<int,int>>> pq;
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                int sum=nums1[i]+nums2[j];
-                if(pq.size()<k) pq.push({sum,{nums1[i],nums2[j]}});
-                else if(sum<pq.top().first){
-                    pq.pop();
-                    pq.push({sum,{nums1[i],nums2[j]}});
-                } else break;
+        vector<vector<int>>ans;
+        priority_queue<pair<int,pair<int,int>>> sol;
+        for(int i =0;i<nums1.size();i++)
+        {
+            for(int j=0;j<nums2.size();j++)
+            {
+                int sum = nums1[i]+nums2[j];
+                if(sol.size()<k)
+                {
+                    sol.push({sum,{nums1[i],nums2[j]}});
+                }
+                else if(sum<sol.top().first)
+                {
+                    sol.pop();
+                    sol.push({sum,{nums1[i],nums2[j]}});
+                }
+                else
+                    break;
             }
         }
-        while(!pq.empty()){
-            ans.push_back({pq.top().second.first,pq.top().second.second});
-            pq.pop();
+        while(!sol.empty())
+        {
+            ans.push_back({sol.top().second.first,sol.top().second.second});
+            sol.pop();
         }
         return ans;
     }
-
 };
