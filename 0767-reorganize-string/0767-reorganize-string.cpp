051d0310e990272e79dd02a70e93d7c9ec8ad93f
Vivek Yadav <vivekcode101@gmail.com>
2023-08-23 13:49:21 +0530

    Time: 4 ms (38.45%), Space: 6.3 MB (46.07%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+         map<int, int>count;
+    for (auto c : s) {
+        count[c]++;
+    }
+        priority_queue<pair<int,int>>pq;
+        for(auto i:count)
+        {
+            pq.push({i.second,i.first});
+        }
+        string ans;
+        
+        pair<int,int>top1st,top2nd;
+        while(!pq.empty())
+        {
+            top1st = pq.top();
+            pq.pop();
+            ans+=top1st.second;
+            if(!pq.empty())
+            {
+                ans+=pq.top().second;
+                top2nd = pq.top();
+                pq.pop();
+                
+                if(top2nd.first>1)
+                {
+                    pq.push({top2nd.first-1,top2nd.second});
+                }
+                
+            }
+            if(top1st.first>1)
+                {
+                    pq.push({top1st.first-1,top1st.second});
+                }
+        }
+        for(int i = 0;i<ans.size();i++)
+        {
+            if(ans[i]==ans[i+1])
+                return "";
+        }
+
+            return ans;
+    }
+};
