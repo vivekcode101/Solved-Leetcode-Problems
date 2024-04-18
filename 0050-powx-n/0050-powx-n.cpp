f6f05efbeb425f87c2bbde037709d16e92b608b4
Vivek Yadav <vyadav@initializ.io>
2024-04-18 11:37:08 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (74.14%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -21,15 +21,22 @@
 // };
 
 
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+//         double res = 1;
+//         while (n) {
+//             if (n % 2) res = n > 0 ? res * x : res / x;
+//             x = x * x;
+//             n /= 2;
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
-    double myPow(double x, int n) {
-        double res = 1;
-        while (n) {
-            if (n % 2) res = n > 0 ? res * x : res / x;
-            x = x * x;
-            n /= 2;
-        }
-        return res;
+    double myPow(double x, int n, double res = 1) {
+        return n ? myPow(x * x, n / 2, n % 2 ? (n > 0 ? res * x : res / x) : res) : res;
     }
 };
